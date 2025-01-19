8b9a479884abdf3e7f006ec228d612a7e3ad7175
혜성 <118062596+answad@users.noreply.github.com>
2025-01-20 08:46:20 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 84 ms, Memory: 12692 KB -BaekjoonHub

--- 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.kt
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.kt
@@ -1,10 +1,14 @@
 fun main() {
     val times = readln().toInt()
-    print(
-        if (times == 0) 0
-        else generateSequence(0L to 1L) { it.second to it.first + it.second }
-            .take(times + 1)
-            .last()
-            .first
-    )
+    var pair = Pair(0L, 1L)
+    when (times) {
+        0 -> print(0)
+        1 -> print(1)
+        else -> {
+            repeat(times - 1) {
+                pair = Pair(pair.second, pair.first + pair.second)
+            }
+            print(pair.second) 
+        }
+    }
 }
