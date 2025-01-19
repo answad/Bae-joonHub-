0ff10f1c586960ee51839085548d3f4debe8512c
혜성 <118062596+answad@users.noreply.github.com>
2025-01-20 00:05:08 +0900

    Update 피보나치 수 2.kt

--- 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.kt
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.kt
@@ -2,7 +2,7 @@ fun main() {
     val times = readln().toInt()
     print(
         if (times == 0) 0
-        else generateSequence(0 to 1) { it.second to it.first + it.second }
+        else generateSequence(0L to 1L) { it.second to it.first + it.second }
             .take(times + 1)
             .last()
             .first
